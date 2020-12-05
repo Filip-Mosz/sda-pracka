2b7dec050e605cad1a76a47bb809bb13054ecfd1
Greg Witczak <gogowitczak@gmail.com>
2020-12-05 10:33:59 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sda-pracka
---
+++ src/main/java/pl/sdacademy/pracka/SdaPrackaApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.pracka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaPrackaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdaPrackaApplication.class, args);
+	}
+
+}
