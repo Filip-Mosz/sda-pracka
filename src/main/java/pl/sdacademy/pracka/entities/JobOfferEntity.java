aca0e3663c3d55209b29935c633de84f5b6a8eb1
Greg Witczak <gogowitczak@gmail.com>
2020-12-05 14:01:07 +0100

    Preparing JobOfferEntity and JobOfferService

--- src/main/java/pl/sdacademy/pracka/entities/JobOfferEntity.java
+++ src/main/java/pl/sdacademy/pracka/entities/JobOfferEntity.java
@@ -1,15 +1,17 @@
 package pl.sdacademy.pracka.entities;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobOfferEntity {
 
     @Id
@@ -17,5 +19,15 @@ public class JobOfferEntity {
     private Long id;
 
     private String name;
+
+    private String companyName;
+
+    private String city;
+
+    private String description;
+
+    private Integer minSalary;
+
+    private Integer maxSalary;
 
 }
---
+++ src/main/java/pl/sdacademy/pracka/services/JobOfferService.java
@@ -0,0 +1,26 @@
+package pl.sdacademy.pracka.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import pl.sdacademy.pracka.entities.JobOfferEntity;
+import pl.sdacademy.pracka.repositories.JobOfferRepository;
+
+import javax.annotation.PostConstruct;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class JobOfferService {
+    private final JobOfferRepository jobOfferRepository;
+
+    @PostConstruct
+    void fillWithDummyData() {
+        log.info("Wypełniam bazę danych przykładowymi ofertami pracy");
+
+        final JobOfferEntity juniorSDA = new JobOfferEntity(null, "Junior Developer", "SDA", "Gdynia", "Ogarniętego juniora szukamy, jesteśmy młodzi i dynamiczni!", 3000, 5000);
+        final JobOfferEntity seniorNordea = new JobOfferEntity(null, "Senior Developer", "Nordea", "Gdańsk", "Szukamy wymiatacza, jesteśmy korpo!", 15000, 20000);
+        jobOfferRepository.save(juniorSDA);
+        jobOfferRepository.save(seniorNordea);
+    }
+}
